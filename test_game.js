80bfe9f9b4bf4702356b9df31c86ca8b6ae9327d
imai4510 <82355251+imai4510@users.noreply.github.com>
2021-05-17 12:33:58 +0900

    Update test_game.js

--- test_game.js
+++ test_game.js
@@ -1,17 +1,19 @@
 newDeck = [11,12,13,14,21,22,23,24,31,32,33,34,41,42,43,44,51,52,53,54,61,62,63,64,71,72,73,74,81,82,83,84,91,92,93,94,101,102,103,104,111,112,113,114,121,122,123,124];
-
+//近似値±３以内ならカードが取れることにしましょう
 console.log("山札確認", newDeck);
 
 m_hands = []
 e_hands = []
 field =[]
 
+
+
 while (m_hands.length < 8) {
-    n = newDeck.length;
-    k = Math.floor(Math.random() * n);
+    n = newDeck.length;//山札の枚数
+    k = Math.floor(Math.random() * n);//山札の枚数をもとに乱数生成
   
-    m_hands.push(newDeck[k]);
-    newDeck.splice(k, 1);
+    m_hands.push(newDeck[k]);//札を増やす
+    newDeck.splice(k, 1);//山札から減らす
 }
 
 console.log("自手札！", m_hands);
@@ -37,3 +39,4 @@ while (field.length < 8) {
 console.log("場札！", field);
 
 console.log("山札再確認", newDeck);
+
