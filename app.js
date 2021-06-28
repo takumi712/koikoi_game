d0d19ca4b1786808729c572f24d6697152164a70
takumi712 <yamazakiyt7@gmail.com>
2021-06-28 10:45:19 +0900

    add
    ルーム作成&参加の基盤作成

--- app.js
+++ app.js
@@ -10,13 +10,29 @@ app.get('/' , function(req, res){
     res.sendFile(__dirname+'/main.html');
 });
 
+room_list = [];
+
 io.on('connection',function(socket){
+    socket.on('create_room',function(room_name){
+        socket.join(room_name);
+        console.log(room_name + " to join")
+        room_list.push(room_name);
+        console.log(room_list)
+    });
+    socket.on('join_room',function(room_name){
+        var find_room;
+        find_room = room_list.indexOf(room_name);
+
+        if(find_room != -1){
+            socket.join(room_name);
+            room_list.splice(find_room,1);
+            console.log(room_name + " to join")
+        }
+
+    });
     socket.on('message',function(msg){
         console.log('message: ' + msg);
         io.emit('message', msg);
-    });
-    socket.on('rename',function(name){
-        console.log(name);
     });
 });
 
